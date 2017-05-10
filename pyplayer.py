024bf42f20b9dce3338dbcdfdd78a2ea8988d428
Sumit Maitra <code@sumitmaitra.com>
2017-05-10 22:16:46 +0000

    separated Display driver out to separate file

--- pyplayer.py
+++ pyplayer.py
@@ -1,24 +1,19 @@
 #!/usr/bin/env python
 import os 
 import sys
-from papirus import Papirus
-from PIL import Image
-from PIL import ImageDraw
-from PIL import ImageFont
 import argparse
 import pygame, time
 import eyed3
 from gpiozero import Button
 from time import sleep
+
+from display import Driver
  
 user = os.getuid()
 if user != 0:
 	print ("Please run script as root")
 	sys.exit()
 
-WHITE = 1
-BLACK = 0
-SIZE = 16
 MODE_PLAY = 100
 MODE_SELECT = 101
 MODE_UPDATE = 102
@@ -63,6 +58,7 @@ class MusicPlayer:
 	def button4Pressed(self, button):
 		if self.mode == MODE_PLAY:
 			self.playNext()
+			self.updateDisplay(self.filenames[self.currentIndex])
 		else:
 			self.next()
 	
@@ -86,9 +82,10 @@ class MusicPlayer:
 
 	def button1Pressed(self, button):
 		if self.mode == MODE_PLAY:
-			self.playNext()
+			self.playPrevious()
+			self.updateDisplay(self.filenames[self.currentIndex])
 		else:
-			self.next()
+			self.previous()
 	
 	def eventloop(self):
 		while(True):
@@ -172,45 +169,6 @@ class MusicPlayer:
 				print(musicFile)
 				count += 1
 		self.driver.write_lines(["%d files indexed!" % count], 20)
-
-class Driver():		
-	papirus = Papirus()
-	line_position=0
-	
-	def write_lines(self, lines, fontsize):
-		# initially set all white background
-		image = Image.new('1', self.papirus.size, WHITE)
-		# prepare for drawing
-		draw = ImageDraw.Draw(image)
-		self.line_position = 0
-		for line in lines:
-			self.line_position += 1
-			self.write_text(draw, line, fontsize)
-
-		self.papirus.display(image)
-		self.papirus.update()
-
-	def write_text(self, draw, text, size):
-		font = ImageFont.truetype('/usr/share/fonts/truetype/freefont/FreeMono.ttf', size)
-		# Calculate the max number of char to fit on line
-		line_size = (self.papirus.width / (size*0.65))
-		current_line = 0
-		text_lines = [""]
-		# Compute each line
-		for word in text.split():
-			# If there is space on line add the word to it
-			if (len(text_lines[current_line]) + len(word)) < line_size:
-				text_lines[current_line] += " " + word
-			else:
-				# No space left on line so move to next one
-				text_lines.append("")
-				current_line += 1
-				text_lines[current_line] += " " + word
-		current_line = self.line_position - 1
-		for l in text_lines:
-			current_line += 1
-			draw.text( (0, ((size*current_line)-size)) , l, font=font, fill=BLACK)
-		self.line_position += (current_line - 1)
 
 def main():
 	pygame.init()
